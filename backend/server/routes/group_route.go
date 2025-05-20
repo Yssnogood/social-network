be98134bf544ce99cf526d5119c2315af456233a
ybellaho <yanis.bellahouel76@gmail.com>
2025-05-20 11:35:06 +0200

    all other route (in comment)

---
+++ backend/server/routes/group_route.go
@@ -0,0 +1,16 @@
+package routes
+
+import (
+	//"social-network/backend/server/handlers"
+	//"github.com/gorilla/mux"
+)
+// UserRoutes
+/*
+func GroupsRoutes(r *mux.Router, groupHandler *handlers.groupHandler) {
+	r.HandleFunc("/api/groups", groupHandler.CreateGroup).Methods("POST")
+	r.HandleFunc("/api/groups/{id}", groupHandler.GetGroup).Methods("GET")
+	r.HandleFunc("/api/groups/{id}", groupHandler.UpdateGroup).Methods("PUT")
+	r.HandleFunc("/api/groups/{id}", groupHandler.DeleteGroup).Methods("DELETE")
+}
+*/
+
---
+++ backend/server/routes/notification_route.go
@@ -0,0 +1,16 @@
+package routes
+
+import (
+	//"social-network/backend/server/handlers"
+	//"github.com/gorilla/mux"
+)
+// UserRoutes
+/*
+func NotificationsRoutes(r *mux.Router, notificationHandler *handlers.notificationHandler) {
+	r.HandleFunc("/api/notifications", notificationHandler.CreateNotification).Methods("POST")
+	r.HandleFunc("/api/notifications/{id}", notificationHandler.GetNotification).Methods("GET")
+	r.HandleFunc("/api/notifications/{id}", notificationHandler.UpdateNotification).Methods("PUT")
+	r.HandleFunc("/api/notifications/{id}", notificationHandler.DeleteNotification).Methods("DELETE")
+}
+*/
+
