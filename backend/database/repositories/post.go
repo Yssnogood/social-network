fddfcc642af069a7dfa8396acb83f1751faac605
Theo Ichane <texio_@Mac.localdomain>
2025-06-12 14:05:52 +0200

    like ok

--- backend/database/repositories/post.go
+++ backend/database/repositories/post.go
@@ -2,6 +2,9 @@ package repository
 
 import (
 	"database/sql"
+	"fmt"
+	"slices"
+	"time"
 
 	"social-network/backend/app/services"
 	"social-network/backend/database/models"
@@ -78,7 +81,7 @@ func (r *PostRepository) GetByID(id int64) (*models.Post, error) {
 	return post, nil
 }
 
-func (r *PostRepository) GetPosts(ps *services.PostService) ([]map[string]any, error) {
+func (r *PostRepository) GetPosts(ps *services.PostService, curr_user *models.User) ([]map[string]any, error) {
 	var posts []map[string]any
 	stmt, err := r.db.Prepare(`
 		SELECT id, user_id, content, image_path, privacy_type, created_at, updated_at
@@ -109,10 +112,13 @@ func (r *PostRepository) GetPosts(ps *services.PostService) ([]map[string]any, e
 		if err != nil {
 			return nil, err
 		}
+		likes, _ := ps.GetLikes(post.ID)
 		user, _ := ps.GetPostAuthor(post)
 		posts = append(posts, map[string]any{
-			"post": post,
-			"user": user.Username,
+			"post":       post,
+			"user":       user.Username,
+			"like":       len(likes),
+			"user_liked": slices.Contains(likes, curr_user.Username),
 		})
 	}
 	return posts, nil
@@ -137,6 +143,65 @@ func (r *PostRepository) Update(post *models.Post) error {
 		post.UpdatedAt,
 		post.ID,
 	)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// ADD or DELETE a Like on a post
+func (r *PostRepository) Like(post_id int64, user_id int64) string {
+	stmt, err := r.db.Prepare(`
+		SELECT id 
+		FROM post_like
+		WHERE post_id = ? AND user_id = ?;
+	`)
+	if err != nil {
+		return ""
+	}
+	var id int64
+	err = stmt.QueryRow(post_id, user_id).Scan(&id)
+	if err != nil {
+		r.AddLike(post_id, user_id)
+		return "liked"
+	}
+	r.DeleteLike(post_id, user_id)
+	return "disliked"
+}
+
+// Like a Post
+func (r *PostRepository) AddLike(post_id int64, user_id int64) {
+	stmt, err := r.db.Prepare(`
+	INSERT INTO post_like (post_id,user_id,created_at)
+	VALUES(?,?,?);
+	`)
+	if err != nil {
+		return
+	}
+	defer stmt.Close()
+	_, err = stmt.Exec(
+		post_id,
+		user_id,
+		time.Now())
+	if err != nil {
+		fmt.Println(err)
+		r.DeleteLike(post_id, user_id)
+		return
+	}
+}
+
+// Delete a post like from the database
+func (r *PostRepository) DeleteLike(post_id int64, user_id int64) error {
+	stmt, err := r.db.Prepare(`
+		DELETE FROM post_like WHERE post_id = ? AND user_id = ?;
+	`)
+	if err != nil {
+		return err
+	}
+	defer stmt.Close()
+
+	_, err = stmt.Exec(post_id, user_id)
 	if err != nil {
 		return err
 	}
